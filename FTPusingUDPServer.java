8113c61492f7ddc0aa8e011b0e90df892cd70574
the-ar03 <113880972+the-ar03@users.noreply.github.com>
2022-09-19 19:13:17 +0530

    Update FTPusingUDPServer.java

--- FTPusingUDPServer.java
+++ FTPusingUDPServer.java
@@ -1,3 +1,4 @@
+// FTPusingUDPServer
 import java.net.*;
 import java.io.*;
 public class FTPusingUDPServer{
@@ -21,5 +22,22 @@ msg.getBytes(0,msg.length(),data,0);
 DatagramSocket ds = new DatagramSocket(8000);
 DatagramPacket dp=new DatagramPacket(data,data.length,sa,8001);
 ds.send(dp);
+}
+}
+
+
+
+// FTPusingUDPClient
+import java.net.*;
+import java.io.*;
+public class FTPusingUDPClient{
+public static void main(String[] args) throws Exception{
+InetAddress sa=InetAddress.getLocalHost();
+byte data[]=new byte[1024];
+DatagramSocket ds = new DatagramSocket(8001);
+DatagramPacket dp=new DatagramPacket(data,data.length);
+ds.receive(dp);
+String msg=new String(dp.getData(),0,0,dp.getLength());
+System.out.println("Received data : " + msg);
 }
 }
