df438858a9fc653d2ea69fa4768e6e2fc3626188
the-ar03 <113880972+the-ar03@users.noreply.github.com>
2022-09-19 19:14:59 +0530

    Update UDPServer.java

--- UDPServer.java
+++ UDPServer.java
@@ -1,5 +1,6 @@
 // creation of UDP server
 
+// UDPServer
 import java.net.*;
 import java.io.*;
 public class UDPServer{
@@ -26,6 +27,26 @@ b=s.getBytes();
 DatagramPacket dp=new DatagramPacket(b,s.length(),id,client);
 ds.send(dp);
 }
+}
+}
+}
+
+
+// UDPClient
+import java.net.*;
+import java.io.*;
+public class UDPClient{
+public static int client=789;
+public static void main(String args[]) throws IOException{
+DatagramSocket ds=new DatagramSocket(client);
+byte b[]=new byte[1024];
+System.out.println("client....receiving....");
+while(true){
+DatagramPacket dp=new DatagramPacket(b,b.length);
+ds.receive(dp);
+String s=new String(dp.getData(),0,dp.getLength());
+if(s.equals("end")) break;
+else System.out.println(s);
 }
 }
 }
