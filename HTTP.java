04b1252a96148c5c0168de59c0c642b07d54a47a
the-ar03 <113880972+the-ar03@users.noreply.github.com>
2022-09-19 19:41:54 +0530

    Add files via upload

---
+++ HTTP.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.net.*;
+public class HTTP{
+public static void main(String args[])throws IOException{
+URL url=new URL("http://www.google.com/");
+URLConnection conn=url.openConnection();
+conn.connect();
+InputStreamReader content= new InputStreamReader(conn.getInputStream());
+FileWriter f=new FileWriter ("abc.html");
+for(int i=0;i!=-1;i= content.read()){
+f.write((char)i);
+}
+}
+}
