70a2c89d515c6031a072a8419fe084b45452287b
the-ar03 <113880972+the-ar03@users.noreply.github.com>
2022-09-19 13:57:37 +0530

    Add files via upload

---
+++ ChatClient.java
@@ -0,0 +1,17 @@
+import java.net.*;
+import java.io.*;
+public class ChatClient{
+public static void main(String arg[])throws Exception{
+Socket s=new Socket(InetAddress.getLocalHost(),4000);
+BufferedReader br=new BufferedReader(new 
+InputStreamReader(s.getInputStream()));
+BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+PrintWriter out=new PrintWriter(s.getOutputStream(),true);
+String input,t;
+while(true){
+System.out.println("client");
+out.println("client:"+in.readLine());
+System.out.println(br.readLine());
+}
+}
+}
