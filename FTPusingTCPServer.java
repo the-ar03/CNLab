203592379c2e4027f28f0a5aa47662b1ecf8e5ae
the-ar03 <113880972+the-ar03@users.noreply.github.com>
2022-09-19 19:11:44 +0530

    Update FTPusingTCPServer.java

--- FTPusingTCPServer.java
+++ FTPusingTCPServer.java
@@ -1,3 +1,4 @@
+// FTPusingTCPServer
 import java.net.*;
 import java.io.*;
 public class FTPusingTCPServer extends Thread{
@@ -26,6 +27,26 @@ out.println(contents);
 }
 catch(Exception e){
 System.out.println(e);
+} 
+} 
+}
+
+
+
+
+// FTPusingTCPClient
+import java.net.*;
+import java.io.*;
+public class FTPusingTCPClient {
+public static void main(String args[]){
+try{
+Socket s=new Socket(InetAddress.getLocalHost(),4000);
+BufferedReader br=new BufferedReader(new InputStreamReader(s.getInputStream()));
+String str;
+while((str=br.readLine())!=null)
+System.out.println(str);}
+catch(Exception e){
+System.out.println("The connection to the server has been reset");
 } 
 } 
 }
