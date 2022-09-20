112694bfc6df40f724b2be5c8719d3a3ec13725c
the-ar03 <113880972+the-ar03@users.noreply.github.com>
2022-09-20 12:33:23 +0530

    Update Ping.java

--- Ping.java
+++ Ping.java
@@ -20,3 +20,13 @@ java.util.Date date=new java.util.Date();
 System.out.println(date);
 }
 }
+
+
+// Ping Command Algorithm:
+// 1. Import necessary packages.
+// 2. Initialize class Ping.
+// 3. getRuntime().exec(Command) returns the runtime object.
+// 4. BufferedReader function read each byte line to line.
+// 5. printStackTrace() function used to handle exceptions and errors.
+// 6. Enter the required website to ping.
+// 7. Stop the program.
