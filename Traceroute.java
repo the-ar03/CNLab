d0f249087b99a7b18e6652056c585d01a5bfd88b
the-ar03 <113880972+the-ar03@users.noreply.github.com>
2022-09-20 12:28:50 +0530

    Update Traceroute.java

--- Traceroute.java
+++ Traceroute.java
@@ -20,3 +20,11 @@ String ip = "www.drranurekha.com";
 runSystemCommand("tracert " + ip);
 }
 }
+
+
+// Traceroute Algorithm:
+// 1. Import all the necessary packages.
+// 2. Create the class Traceroute.
+// 3. the getRuntime() command returns the runtime object associated with current java application.
+// 4. Which website you want to see the trace enters the command to the server.
+// 5. Stop the program.
