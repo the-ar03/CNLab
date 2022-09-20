8a72fe3209f7524874fe6aab3a3b352a03b0b757
the-ar03 <113880972+the-ar03@users.noreply.github.com>
2022-09-20 11:41:23 +0530

    Update IPAddress.java

--- IPAddress.java
+++ IPAddress.java
@@ -9,3 +9,16 @@ public class IPAddress{
         System.out.println("Local Host Name: " + hostname);
     }
 }
+
+
+
+// IPAddress Algorithm:
+// 1. Start
+// 2. Import the required packages.
+// 3. Create a class IPAddress.
+// 4. Inatialize InetAddredd class and create object addr.
+// 5. Get IPAddress of the system using getHostAddress() method.
+// 6. Print the IPAddress.
+// 7. Get hostname of the system using getHostName() method
+// 8. Print the hostname.
+// 9. Stop
